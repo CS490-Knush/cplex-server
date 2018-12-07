faea2a34dc53b8e8fb64038de478cfd8adde219f
Anushree Agrawal <anushree.agrawal27@gmail.com>
2018-12-07 00:20:19 -0500

    Try calling CPLEX from python

--- cplex_server/controllers/default_controller.py
+++ cplex_server/controllers/default_controller.py
@@ -1,17 +1,37 @@
 import connexion
 import six
+import json
+import datetime
+import subprocess
 
 from cplex_server.models.bi_job_matrix import BIJobMatrix  # noqa: E501
 from cplex_server.models.i_matrix import IMatrix  # noqa: E501
 from cplex_server.models.job_status import JobStatus  # noqa: E501
 from cplex_server.models.parameters import Parameters  # noqa: E501
 from cplex_server import util
+from multiprocessing import Process
 
 # dict: id: {status:status, bimatrix: [], imatrix: []}
-JOBS = {4: {'status': 'processing', 'bijobmatrix': [7, 8], 'imatrix': [[1, 0],[0, 1], [0, 0], [0,0]]}}
+
+class JobInfo():
+
+
+    def __init__(self, id, status, data_file="", output_file=""):
+        self.status = JobStatus(id, status)
+        self.data_file = data_file
+        self.output_file = output_file
+
+    def set_data_file(self, data_file):
+        self.data_file = data_file
+
+    def set_output_file(self, output_file):
+        self.output_file = output_file
+
+JOBS = {4: JobInfo(4, "processing", "cs490.dat", "output.txt")}
+curr_id = 1
 
 def find_status_by_job_id(jobId):  # noqa: E501
-    return JOBS[jobId]['status']
+    return JOBS[jobId].status # get status from job_status model
 
 
 def get_bi_job_matrix(jobId):  # noqa: E501
@@ -34,5 +54,45 @@ def submit_job(body):  # noqa: E501
     """
     if connexion.request.is_json:
         body = Parameters.from_dict(connexion.request.get_json())  # noqa: E501
-        
+        new_id = write_to_data_file(body)
+
+
     return 'do some magic!'
+
+def write_to_data_file(body):
+    global curr_id
+    curr_id += 1
+    global JOBS
+    filename = 'data_files/{id}_{date:%Y-%m-%d_%H-%M-%S}.dat'.format(id=curr_id, date=datetime.datetime.now())
+    with open(filename, 'w') as f:
+        f.write('Flows = {')
+        for flow in body.flows:
+            f.write('"%s",' % flow)
+        f.write("};\n")
+
+        f.write('Jobs = {')
+        for job in body.jobs:
+            f.write('"%s",' % job)
+        f.write("};\n")
+
+        f.write("numConstraints = %d;\n" % body.num_constraints)
+
+        f.write("A = %s;\n" % str(body.a))
+
+        f.write("C = %s;\n" % str(body.c))
+    JOBS[curr_id] = JobInfo(curr_id, "processing", filename, "")
+    p = Process(target=run_cplex_job, args=(filename, curr_id))
+    p.start()
+    return curr_id
+
+def run_cplex_job(data_file, curr_id):
+    s = subprocess.check_output([return_cplex_loc(), return_data_file_loc(data_file)])
+    print(s)
+
+def return_cplex_loc():
+    return './CPLEX_Studio128/opl/bin/x86-64_linux/oplrun'
+
+def return_data_file_loc(data_file):
+    return 'cplex-server/data-files/%s' % data_file
+
+
